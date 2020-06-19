f310614a7f97894df30eafe651d7d395c13b3ea5
Noura <nouraellm@gmail.com>
2020-06-19 12:00:33 +0200

    [ADD] awesome_tshirt: bafienistalkingtoyou using rpc

--- awesome_tshirt/static/src/js/homemenu.js
+++ awesome_tshirt/static/src/js/homemenu.js
@@ -8,10 +8,23 @@ odoo.define('awesome_tshirt.HomeMenu', function (require) {
 
     HomeMenu.include({
 
+        willStart: function () {
+            var promise = this._rpc({
+                route: '/awesome_tshirt/bafienistalkingtoyou',
+            }).then((data) => {
+                this.msg = data;
+            });
+            return Promise.all([
+                this._super.apply(this, arguments),
+                promise
+                ]);
+        },
         _render: function () {
             this._super.apply(this, arguments);
             this.$('.o_custom_message').remove();
-            this.$el.prepend(Qweb.render('HomeMenu.CustomMessage'));
+            this.$el.prepend(Qweb.render('HomeMenu.CustomMessage', {
+                message: this.msg
+            }));
         },
     });
 });
